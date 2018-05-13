fbcdd33a84c401547b98725be3d16d245daf6506
Songzhiguo <Fu_shide@163.com>
2018-05-13 22:32:57 +0800

    Site updated: 2018-05-13 22:32:56

--- script/search.js
+++ script/search.js
@@ -15,7 +15,7 @@ var searchFunc = function(path, search_id, content_id) {
             //ID选择器
             var $input = document.getElementById(search_id);
             var $resultContent = document.getElementById(content_id);
-            $input.addEventListener('input', function(){
+            $(document).on('input', $($input), function(){
                 var str='<ul class=\"search-result-list\">';
                 var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
                 $resultContent.innerHTML = "";
